041e8142283d3a1f691a379df4cbd218a978a73d
Johan Svensson <johan@atomicplayboy.net>
2016-03-24 15:13:28 +0100

    Split it up into sub-components

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -35,4 +35,39 @@ describe('Voting', () => {
     expect(votedWith).to.equal('Alien')
   })
 
+  it('disables buttons when the user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Superman", "Batman"]}
+        hasVoted="Batman" />
+    )
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
+
+    expect(buttons.length).to.equal(2)
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true)
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true)
+  })
+
+  it('adds a label to the entry voted for', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Superman", "Batman"]}
+        hasVoted="Batman" />
+    )
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
+
+    expect(buttons[1].textContent).to.contain('Voted')
+  })
+
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    )
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
+
+    expect(buttons.length).to.equal(0)
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner)
+    expect(winner).to.be.ok
+    expect(winner.textContent).to.contain('Trainspotting')
+  })
+
 })
