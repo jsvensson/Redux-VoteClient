7656930939745d6073925f481789f2d773586eda
Johan Svensson <johan@atomicplayboy.net>
2016-04-04 16:39:40 +0200

    Test vote counting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {List} from 'immutable'
+import {List, Map} from 'immutable'
 
 const pair = List.of('Alien', 'Batman')
+const tally = Map({'Alien': 8, 'Batman': 5})
 
 export default React.createClass({
   render: function() {
